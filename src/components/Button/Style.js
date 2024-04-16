1542ef15918f72e0e4929a1ed68e1992dbe1ede9
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-04-16 19:30:14 -0300

    Criaçao das estruturas

---
+++ src/components/Button/Style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const StyledOutline = styled.article`
+
+`
+
+export {StyledOutline}
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { OutlineButton, RaisedButton } from "../components/Button/Button";
+
+
+const LandingPage = () => (
+  <>
+    <div className="container">
+      <h1>Landing</h1>
+      <OutlineButton>Outline</OutlineButton>
+      <RaisedButton>RAISED</RaisedButton>
+    </div>
+  </>
+)
+
+export default LandingPage;
