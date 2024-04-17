b8eb32f2d27be7e427796fb50d88adb26f539621
rodrigosmes <126490594+rodrigosmes@users.noreply.github.com>
2024-04-16 23:10:41 -0300

    Components-ButtonsCriation

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,29 @@
+import { OutlineButtonStyled, SecondLineButtonStyled, ThirdLineButtonStyled } from "./Style";
 
-export const OutlineButton = (props) => (
-    <>
-    </>
-)
+export const OutlineButton = (props) => {
+    return (
+    <OutlineButtonStyled>
+        <button>{props.children}</button>
+    </OutlineButtonStyled>
 
+  );   
+};
 
+export const SecondLineButton = (props) => {
+    return (
+    <SecondLineButtonStyled>
+        <button>{props.children}</button>
+    </SecondLineButtonStyled>
+
+  );   
+};
+
+export const ThirdLineButton = (props) => {
+    return (
+    <ThirdLineButtonStyled>
+        <button>{props.children}</button>
+    </ThirdLineButtonStyled>
+
+  );   
+};
 
--- src/components/Button/Style.js
+++ src/components/Button/Style.js
@@ -1,7 +1,48 @@
 import styled from "styled-components";
 
-const StyledOutline = styled.article`
+const OutlineButtonStyled = styled.article`
+button{
+    font-size: 13px;
+    color: var(--neutral); 
+    background-color: transparent;
+    cursor: pointer;
+    padding: 6px 20px;
+    border: none;
+    &:hover{
+        border-bottom: 2px solid #fff;  
+    }
+    
 
+}
+`
+const SecondLineButtonStyled = styled.article`
+
+button{
+    font-size: 13px;
+    color: var(--contrast);
+    background-color: var(--neutral);
+    cursor: pointer;
+    padding: 6px 20px;
+    border: none;
+    border-radius: 20rem;
+    
+
+}
 `
 
-export {StyledOutline}
+const ThirdLineButtonStyled = styled.article`
+
+button{
+    font-size: 13px;
+    color: var(--neutral);
+    background-color: var(--primaria);
+    cursor: pointer;
+    padding: 6px 20px;
+    border: none;
+    border-radius: 20rem;
+    
+
+}
+`
+
+export {OutlineButtonStyled, SecondLineButtonStyled,ThirdLineButtonStyled};
