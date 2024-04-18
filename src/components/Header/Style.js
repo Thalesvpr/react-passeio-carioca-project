d2f28c57b14a6347a708d01008a586a7b375e5a3
Erguimaraes30 <160931118+Erguimaraes30@users.noreply.github.com>
2024-04-17 21:54:27 -0300

    Header e Footer

---
+++ src/components/Header/Style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.article`
+    height: 100px;
+    width: 100%;
+    background-color: var(--primaria);
+    position: fixed;
+    z-index: 10;
+    div.content{
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-inline: 50px;
+        
+    }
+    div.nav{
+        display: flex;
+    }
+    h1{
+        color: white;
+        padding-left: 65px;
+       
+    }
+`
+
+
+
+
+export {HeaderStyled}
