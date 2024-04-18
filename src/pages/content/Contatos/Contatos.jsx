525e4415251663e0da44a4c021de26202ec012b2
rodrigosmes <126490594+rodrigosmes@users.noreply.github.com>
2024-04-17 22:29:24 -0300

    ADD Contato

---
+++ src/pages/content/Contatos/Contatos.jsx
@@ -0,0 +1,16 @@
+import SectionBase from "../../../components/SectionBase/SectionBase";
+import { ContatoSectionStyled } from "./Style";
+
+export const ContatoSection = () => (
+    <SectionBase>
+        <ContatoSectionStyled>
+            <div className="ContatosSection">
+            <h1>Precisa entrar em contato conosco?</h1>
+            <h2>Tem perguntas ou sugestões? Nossa equipe está pronta para ajudar.</h2>
+            </div>
+        </ContatoSectionStyled>
+
+
+    </SectionBase>
+
+)
---
+++ src/pages/content/Contatos/Style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const ContatoSectionStyled = styled.article`
+
+div.ContatoSection {
+    color: var(--contrast);
+    background-color: blue;
+
+}
+
+`
+export {ContatoSectionStyled};
