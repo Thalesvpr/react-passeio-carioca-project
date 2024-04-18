243e6f180f9c86c0770fbf1821b6fb5821171241
EduardoParga <edu.f.parga@gmail.com>
2024-04-17 22:06:51 -0300

    Clean-landing-page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import PasseiosSection from "./content/Passeios/Passeios";
-import ExploreSection from "./content/Explore/Explore";
+
 
 
 
@@ -8,8 +7,8 @@ import ExploreSection from "./content/Explore/Explore";
 const LandingPage = () => (
     <div className="container">
       <h1>Landing</h1>
-      <PasseiosSection></PasseiosSection>
-      {/* <ExploreSection></ExploreSection> */}
+     
+      
     </div>
 
 )
