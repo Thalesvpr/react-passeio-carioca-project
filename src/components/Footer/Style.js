96384f15cbe828fcfeffca7e21afcda0bd79aff8
Erguimaraes30 <160931118+Erguimaraes30@users.noreply.github.com>
2024-04-18 16:43:17 -0300

    Footer

--- src/components/Footer/Style.js
+++ src/components/Footer/Style.js
@@ -7,6 +7,7 @@ const FooterStyled = styled.article`
     z-index: 10;
     div.content-socials{
         color: white;
+        
        
     }
     div.nav{
@@ -15,11 +16,14 @@ const FooterStyled = styled.article`
     }
     div.socials-links{
         margin: 10px;
-        
+        display: flex;
+        align-items: center;
+        padding-inline: 50px;
     }
+        
     div.content{
         background-color: var(--primaria);
-        height: 100%;
+        height: 600px;
         display: flex;
         justify-content: space-between;
         padding: 50px;
@@ -27,7 +31,7 @@ const FooterStyled = styled.article`
     }
     div.box{
         height: 100px;
-        background-color: white;
+        background-color: var(--secundaria);
         border-radius: 16px;
         margin-bottom: 20px;
     }
@@ -37,7 +41,34 @@ const FooterStyled = styled.article`
     }
     div.boxlinks{
         background-color: white;
+        height: 48px;
+        width: 48px;
+        border-radius: 13px;
+        margin-right: 10px;
     }
+    div.content-marcadagua{
+        color: var(--neutral);
+        display: flex;
+        align-items: center;
+        padding-inline: 50px;
+        margin-bottom: 10px;
+    }
+    div.marcadagua{
+        background-color: var(--neutral);
+        height: 48px;
+        width: 48px;
+        border-radius: 13px;
+        display: flex;
+        margin-right: 10px;
+    }
+    div.text{
+        display: block;
+    }
+        
+        
+
+        
+        
         
 `
 
