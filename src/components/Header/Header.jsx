295785a7da46da2960422c6ce2c67e6c55c33590
Erguimaraes30 <160931118+Erguimaraes30@users.noreply.github.com>
2024-05-16 15:30:00 -0300

    MenuBurguer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,20 @@ import { OutlineButton } from "../Button/Button";
 import styled from "styled-components";
 import React, { useState, useEffect } from 'react';
 
+export const ContentLogo = styled.div`
+ @media (max-width: 768px) {
+    display: block; 
+  }
+`
+
 export const BurguerButtom = styled.div`
   transition: opacity 0.3s ease, visibility 0.3s ease;
-    display: none;
+  display: none;
   @media (max-width: 768px) {
     display: block; 
   }
 `;
+
 const Menu = styled.div`
   /* position: fixed; */
   opacity: ${({ isVisible }) => isVisible ? '1' : '0'};
@@ -17,77 +24,79 @@ const Menu = styled.div`
   transition: opacity 0.3s ease, visibility 0.3s ease;
   transition: all 1s ease;
 `;
+
 export const Nav = styled.div`
+  background-color: var(--destaque);
+  padding: 10px;
+  padding-inline: 15px;
+  padding-block: 30px;
+  border-radius: 20px;
   transition: opacity 0.3s ease, visibility 0.3s ease;
   @media (max-width: 768px) {
-    display: none; 
+    display: ${({ isNavOpen }) => isNavOpen ? 'flex' : 'none'};
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    position: absolute;
+    right: 25px;
+    bottom: -200px; 
   }
-  display: ${({isNavOpen})=> isNavOpen? 'block' : 'none' };
+  display: flex
 `;
 
 
-  
+  const Header = () => {
+  const [isMenuVisible, setMenuVisible] = useState(true);
+  const [isNavOpen, setNavOpen] = useState(false);
 
-const Header = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
 
-    const [isMenuVisible, setMenuVisible] = useState(true);
-    const [isNavOpen, setNavOpen] = useState(false);
-
-
-    useEffect(() => {
-      const handleScroll = () => {
-        const currentScrollY = window.scrollY;
-        
-        
-        if (currentScrollY > 0) {
-            console.log(false)
-          setMenuVisible(false);
-        } else {
-            console.log(true)
-
-          setMenuVisible(true);
-        }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-    const toggleNav = () => {
-      setNavOpen(!isNavOpen);
+      if (currentScrollY > 0) {
+        setMenuVisible(false);
+      } else {
+        setMenuVisible(true);
+      }
     };
 
+    window.addEventListener('scroll', handleScroll);
 
-  return <HeaderStyled>
-  <div className="content">
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const toggleNav = () => {
+    setNavOpen(!isNavOpen);
+  };
+
+  return (
+    <HeaderStyled>
+      <div className="content">
         <div>
-        <Menu className="content-logo" isVisible={isMenuVisible}>
-    <img className="logo" src={`${process.env.PUBLIC_URL + "imagens/png/LOGO.png"}`}/>
-  <h1>
-        Passeio Carioca
-    </h1>
-    </Menu>
+          <Menu className="content-logo" isVisible={isMenuVisible}>
+            <img className="logo" src={`${process.env.PUBLIC_URL + "imagens/png/LOGO.png"}`} />
+            <h1>Passeio Carioca</h1>
+          </Menu>
         </div>
-      <Nav>
-    <div className="nav">
+        <Nav isNavOpen={isNavOpen}>
         <OutlineButton>Baixe o App</OutlineButton>
         <OutlineButton>Explore</OutlineButton>
         <OutlineButton>Parcerias</OutlineButton>
         <OutlineButton>Fale Conosco</OutlineButton>
-        
-    </div>
-        </Nav> 
+        </Nav>
         <BurguerButtom onClick={toggleNav}>
-        <div className="burguer-buttom">
-          <OutlineButton></OutlineButton>
-        </div>
+          <div className="burguer-buttom">
+            <OutlineButton></OutlineButton>
+          </div>
         </BurguerButtom>
-  </div>
-
-  </HeaderStyled>
-
+      </div>
+    </HeaderStyled>
+  );
 }
 
+ 
+
 export default Header;
+  
