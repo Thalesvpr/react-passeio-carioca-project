2193e4cfa59e016a23bf4a9060a5cab97a58043b
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-05-16 15:10:48 -0300

    --

--- src/pages/content/AnunciantesGuias/AnunciantesGuia.jsx
+++ src/pages/content/AnunciantesGuias/AnunciantesGuia.jsx
@@ -44,17 +44,12 @@ const AnunciantesGuiaSection = () => {
       <GlobalStyles />
       <SectionBase>
         <AnunciantesGuiaSectionStyled>
-          <div className="section-title">
-            <h1>Já pensou em ampliar a visibilidade do seu negócio??</h1>
-          </div>
-          <div className="section-anunciante-content">
-            <div className="content-anunciante-cta">
-              <h2 className="content-anunciante-subtitle">
-                Conecte sua marca com aventureiros
-              </h2>
-              <p className="content-anunciante-text">
-                Aumente a visibilidade da sua empresa.
-              </p>
+          <h1>Já pensou em ampliar a visibilidade do seu negócio??</h1>
+          {/* anunciantes */}
+          <div className="content">
+            <div className="anunciantes">
+              <h2 className="content-anunciante-subtitle">Conecte sua marca com aventureiros</h2>
+              <p className="content-anunciante-text">Aumente a visibilidade da sua empresa.</p>
               <p className="content-anunciante-subtext">
                 Ofereça promoções, se destaque no mapa e acesse dados valiosos
                 sobre seus potenciais clientes.
@@ -65,54 +60,64 @@ const AnunciantesGuiaSection = () => {
                 </NeutralButton>
               </div>
             </div>
-            <div className="section-guia-content">
-              <div className="content-guia-cta">
-                <h2 className="content-guia-subtitle">
-                  Amplie seu alcance online
-                </h2>
-                <p className="content-guia-text">
-                  Conecte-se com turistas e moradores.
-                </p>
-                <p className="content-guia-subtext">
-                  Cadastre-se hoje e comece a oferecer seus passeios guiados
-                  através do Passeio Carioca.
-                </p>
-                <div className="content-guia-button">
-                  <NeutralButton onClick={openGuiaModal}>
-                    Para Guias de Turismo
-                  </NeutralButton>
-                </div>
+            {/* guias */}
+
+            <div className="guia">
+              <h2 className="content-guia-subtitle">
+                Amplie seu alcance online
+              </h2>
+              <p className="content-guia-text">
+                Conecte-se com turistas e moradores.
+              </p>
+              <p className="content-guia-subtext">
+                Cadastre-se hoje e comece a oferecer seus passeios guiados
+                através do Passeio Carioca.
+              </p>
+              <div className="content-guia-button">
+                <NeutralButton onClick={openGuiaModal}>
+                  Para Guias de Turismo
+                </NeutralButton>
               </div>
             </div>
+
+
           </div>
-          <div className="closeOutside" onClick={CloseOpenModal}>
-            <Modal isOpen={openModal} onClose={CloseOpenModal}>
-              <div className="anun-modal" onClick={stopPropagation}>
-                Form Anunciantes
-                
-                  <button className="close-button-a" onClick={CloseOpenModal}>
-                    X
-                  </button>
-                
-              </div>
-            </Modal>
-          </div>
-          <div className="closeOutside" onClick={CloseOpinModal}>
-          <Modal isOpen={opinModal} onClose={CloseOpinModal}>
-            <div className="guia-modal" onClick={stopPropagation}>
-              Form Guia
-              
-                <button className="close-button-g" onClick={CloseOpinModal}>
-                  X
-                </button>
-              
-            </div>
-          </Modal>
-          </div>
+
         </AnunciantesGuiaSectionStyled>
       </SectionBase>
+      <div className="closeOutside" onClick={CloseOpenModal}>
+        <Modal isOpen={openModal} onClose={CloseOpenModal}>
+          <div className="anun-modal" onClick={stopPropagation}>
+            Form Anunciantes
+
+            <button className="close-button-a" onClick={CloseOpenModal}>
+              X
+            </button>
+
+          </div>
+        </Modal>
+      </div>
+      <div className="closeOutside" onClick={CloseOpinModal}>
+        <Modal isOpen={opinModal} onClose={CloseOpinModal}>
+          <div className="guia-modal" onClick={stopPropagation}>
+            Form Guia
+
+            <button className="close-button-g" onClick={CloseOpinModal}>
+              X
+            </button>
+
+          </div>
+        </Modal>
+      </div>
     </>
   );
 };
 
 export default AnunciantesGuiaSection;
+
+
+
+
+
+
+
