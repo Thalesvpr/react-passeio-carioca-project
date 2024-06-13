b98c4d9e64cfe9880a77b980a1447ddc9f9cd3ba
Erguimaraes30 <160931118+Erguimaraes30@users.noreply.github.com>
2024-06-13 05:33:37 -0300

    contato

--- src/pages/content/Contato/Contato.jsx
+++ src/pages/content/Contato/Contato.jsx
@@ -5,48 +5,75 @@ import { NeutralButtonStyled } from "../../../components/Button/Style";
 import { NeutralButton } from "../../../components/Button/Button";
 import { TranslationsContext } from "../../../context/TranslationContext";
 import React, { useState, useEffect, useContext } from "react";
+import emailjs from 'emailjs-com';
+
 
 export const ContatoSection = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+      });
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value
+        });
+      };
+      const handleSubmit = (e) => {
+       e.preventDefault()
+        emailjs.sendForm('service_pjms2vz', 'template_h1dbrfa', e.target, 'YjeYHpwni_REH6jRI')
+          .then((result) => {
+              console.log(result.text);
+              alert('Email enviado com sucesso!');
+          }, (error) => {
+              console.log(error.text);
+              alert('Ocorreu um erro ao enviar o email.');
+          });
 
+      };
+    
     const {locale}= useContext(TranslationsContext);
 
-const translations = {
-    en: {
-        contato: "Need to get in touch with us?",
-        enviar: "Send",
-        titulo1: "Title",
-        escreva: "Write your text",
-        titulo2: "Title",
-    },
-    pt: {
-        contato: "Precisa entrar em contato com a gente?",
-        enviar: "Enviar",
-        titulo1: "Título",
-        escreva: "Escreva seu Título",
-        titulo2: "Título",
-    },
-    de: {
-        contato: "Müssen Sie uns kontaktieren?",
-        enviar: "Senden",
-        titulo1: "Titel",
-        escreva: "Schreiben Sie Ihren Text",
-        titulo2: "Titel"
-    },
-    fr: {
-        contato: "Besoin de nous contacter?",
-        enviar: "Envoyer",
-        titulo1: "Titre",
-        escreva: "Écrivez votre texte",
-        titulo2: "Titre",
-    },
-    es: {
-        contato: "¿Necesitas ponerte en contacto con nosotros?",
-        enviar: "Enviar",
-        titulo1: "Título",
-        escreva: "Escribe tu texto",
-        titulo2: "Título"
-    }
-}
+    const translations = {
+        en: {
+            contato: "Need to get in touch with us?",
+            ph: "Write here",
+            tituloNome: "Full Name",
+            tituloEmail: "Contact Email",
+            botao: "Send"
+        },
+        pt: {
+            contato: "Precisa entrar em contato com a gente?",
+            ph: "Escreva aqui",
+            tituloNome: "Nome completo",
+            tituloEmail: "Email de contato",
+            botao: "Enviar"
+        },
+        de: {
+            contato: "Müssen Sie uns kontaktieren?",
+            ph: "Hier schreiben",
+            tituloNome: "Vollständiger Name",
+            tituloEmail: "Kontakt E-Mail",
+            botao: "Senden"
+        },
+        fr: {
+            contato: "Besoin de nous contacter?",
+            ph: "Écrivez ici",
+            tituloNome: "Nom complet",
+            tituloEmail: "E-mail de contact",
+            botao: "Envoyer"
+        },
+        es: {
+            contato: "¿Necesitas ponerte en contacto con nosotros?",
+            ph: "Escribe aquí",
+            tituloNome: "Nombre completo",
+            tituloEmail: "Correo de contacto",
+            botao: "Enviar"
+        }
+    };
+    
 
 
 const texts = translations[locale];
@@ -55,20 +82,37 @@ const texts = translations[locale];
         <SectionBase>
             <ContatoSectionStyled>
                 <div className="section-content">
+                    <form onSubmit={handleSubmit}>
+
                     <div className="content-form">
                         <h1>{texts.contato}</h1>
                         <InputField
-                            placeholder= {texts.nome}
-                            titulo={texts.escreva}
-                        />
+                            name = "name"
+                            onChange={handleChange}
+                            value={formData.name}
+                            placeholder= {texts.ph}
+                            titulo={texts.tituloNome}
+                            />
                         <InputField
-                            placeholder={texts.email}
+                            name = "email"
+                            onChange={handleChange}
+                            value={formData.email}
+                            placeholder={texts.ph}
+                            titulo={texts.tituloEmail}
+                            />
+                        {/* <InputField
+                            name = "message"
+                            onChange={handleChange}
+                            value={formData.message}
+                            placeholder={texts.message}
                             titulo={texts.escreva}
-                        />
-                        <NeutralButton>
-                            {texts.enviar}
+                            /> */}
+
+                        <NeutralButton type="submit">
+                            {texts.botao}
                         </NeutralButton>
                     </div>
+                            </form>
                     {/* <img className="lapa" src={`${process.env.PUBLIC_URL}imagens/png/lapa.png`}/> */}
                 </div>
                     <img className="lapa" src={`${process.env.PUBLIC_URL}imagens/png/lapa.png`}/>
