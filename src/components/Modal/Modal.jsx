cb3559dacec305e86e28bde73873c0d2d6a23869
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-06-13 06:07:35 -0300

    ajuste modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import ModalStyled from './Style'
-import { BiX } from 'react-icons/bi'
+import React, { useEffect } from 'react';
+import ModalStyled from './Style';
+import { BiX } from 'react-icons/bi';
 
 export default function Modal(props) {
+  useEffect(() => {
+    if (props.isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
 
-    if (props.isOpen)  
-    return (
+    // Cleanup function to restore the original overflow style
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [props.isOpen]);
+
+  if (!props.isOpen) {
+    return null;
+  }
+
+  return (
     <ModalStyled>
-            <div className='modal'>
-                <button className="close" onClick={props.onClose}><BiX/></button>
-            {props.children}
-            </div>
+      <div className="modal">
+        <button className="close" onClick={props.onClose}><BiX /></button>
+        {props.children}
+      </div>
     </ModalStyled>
-    )
-    
-    
-
-    return null
+  );
 }
-
