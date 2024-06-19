00f3e6782b2755959652e82b5838b55f49485d89
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-06-19 19:07:30 -0300

    aa

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import DownloadAppPage from './pages/DownloadApp/DownloadAppPage';
 import NotFoundPage from './pages/PageNotFound/NotFoundPage';
+import { createRoot } from 'react-dom/client';
 
 
 
 
-const basename = true ? "https://thalesvpr.github.io/react-passeio-carioca-project/" : "/";
-console.log(process.env.PUBLIC_URL)
-ReactDOM.render(
-  <Router
-  
-    basename={basename}
-  >
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/download-app-link" element={<DownloadAppPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  </Router>,
-  document.getElementById('root')
+// ReactDOM.render(
+//   <Router
+
+//     basename={basename}
+//   >
+//     <Routes>
+//     </Routes>
+//   </Router>,
+//   document.getElementById('root')
+// );
+
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/download-app-link" element={<DownloadAppPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
 );
